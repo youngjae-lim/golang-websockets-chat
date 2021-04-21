d8a0e16645d1402f158f88dc64fd5bc235fe6f6b
Youngjae <limyoungjae@gmail.com>
2021-04-21 14:54:41 -0400

    fixed an issue where online uuser list is not updated when someone leaves the chat

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -113,6 +113,14 @@ func ListenToWsChannel() {
 			response.Action = "list_users"
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+
+		case "left":
+			response.Action = "list_users"
+			delete(clients, e.Conn)
+			users := getUserList()
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+
 		}
 
 		// response.Action = "Get here"
