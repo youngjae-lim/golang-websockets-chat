b045c7c4a78f4317f70a07cab0260b5c387c6c0e
Youngjae <limyoungjae@gmail.com>
2021-04-21 12:30:09 -0400

    improved websocket endpoint and added listener for websocket

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/youngjae-lim/golang-websockets-chat/internal/handlers"
 )
 
 func main() {
 	mux := routes()
+
+	log.Println("Starting channel listener...")
+	go handlers.ListenToWsChannel()
 
 	log.Println("Starting web server on port 8080...")
 
