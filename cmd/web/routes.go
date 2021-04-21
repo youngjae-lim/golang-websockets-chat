9047bd25b41741601b154092d3abae02810bc8d9
Youngjae <limyoungjae@gmail.com>
2021-04-20 22:20:50 -0400

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/bmizerany/pat"
+	"github.com/youngjae-lim/golang-websockets-chat/internal/handlers"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+
+	return mux
+
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/youngjae-lim/golang-websockets-chat
+
+go 1.16
+
+require (
+	github.com/CloudyKit/jet/v6 v6.1.0 // indirect
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
